92ceeca995642d714ad5d3732415d9474dad3091
Kimberly Villatoro <kimsvillatoro@gmail.com>
2022-09-08 22:40:20 -0400

    add digital clock

--- js/main.js
+++ js/main.js
@@ -4,13 +4,31 @@ const hoursHand = document.getElementById('hours-hand')
 
 
 function currentTime() {
-    const date = new Date();
-    const hh = date.getHours();
-    const mm = date.getMinutes();
-    const ss = date.getSeconds();
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    let session = "AM";
+
+    if(hh == 0) {
+      hh = 12;
+    }
+    if(hh > 12){
+      hh = hh - 12;
+      session = "PM";
+   }
+
+    hh = (hh < 10) ? "0" + hh : hh;
+    mm = (mm < 10) ? "0" + mm : mm;
+    ss = (ss < 10) ? "0" + ss : ss;
+
+    let time = hh + ":" + mm + ":" + ss + " " + session;
 
     const timeInterval = 6;
 
+    document.getElementById("digital-clock").innerText = time;
+    setTimeout(function(){ currentTime() }, 1000);
 
     secondsHand.style.transform = 'rotate(' + (ss * timeInterval) + 'deg)'
     minutesHand.style.transform = 'rotate(' + (mm * timeInterval + ss / 10) + 'deg)'
